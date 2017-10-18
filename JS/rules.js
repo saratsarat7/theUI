b9dd0ec9821e265b079e2175960a972cb4acb6c1
Sarat <31168235+saratsarat7@users.noreply.github.com>
2017-10-18 15:30:33 +0530

    Rule File Creation
    Download rule file from text input

--- JS/rules.js
+++ JS/rules.js
@@ -1,10 +1,75 @@
 function process_rule(value) {
     // var value = document.getElementById(id).value;
-    document.getElementById('test').value=value;
+    // document.getElementById('test').value=value;
     // document.write(value);
-    document.getElementById("demo").innerHTML = PARSER.parse(value);
+    var parse_json = null;
+    var parse_error = null;
+    try {
+        var parse_json = PARSER.parse(value);
+    } catch (e) {
+        if (e.location !== undefined) {
+            var parse_error = "Line " + e.location.start.line + ", column " + e.location.start.column + ": " + e.message;
+        } else {
+            var parse_error = e.message;
+        }
+    }
+    if (parse_error != null) {
+        document.getElementById("parse_json").innerHTML = parse_error;
+        document.getElementById("error_switch").value = "Y";
+    } else {
+        document.getElementById("parse_json").innerHTML = parse_json;
+        document.getElementById("error_switch").value = "N";
+    }
 }
 
-function write_file() {
-    
+function process_json() {
+    var json_error = null;
+    var json_htlm = null;
+
+    var json_error = document.getElementById("error_switch").value;
+    if (json_error == "Y") {
+        window.alert("Please fix the errors and then create rule file");
+    } else {
+        json_htlm = JSON.parse(document.getElementById("parse_json").innerHTML);
+        create_rule (json_htlm);
+    }
+}
+
+function create_rule(json_rule) {
+    var json_cond = json_rule.condition;
+    var json_out  = json_rule.output;
+    var rule = "";
+    // Looping through rules
+    for(var i = 0; i < json_cond.length; i++) {
+        var obj = json_cond[i];
+        rule = rule.concat("?",obj[0],"=",obj[1]);
+    }
+    // Looping through outputs
+    for(var i = 0; i < json_out.length; i++) {
+        var obj = json_out[i];
+        rule = rule.concat(".",obj[0],"=",obj[1]);
+    }
+    document.getElementById("rule_out").innerHTML = rule;
+    return rule;
+}
+
+function download() {
+    var filename = "rules.dsg";
+    var text = document.getElementById("rule_out").innerHTML;
+    var error = document.getElementById("error_switch").value;
+
+    if (error == "Y") {
+        window.alert("Cant download there is an eroor or rule is empty.");
+    } else {
+        var element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+        element.setAttribute('download', filename);
+
+        element.style.display = 'none';
+        document.body.appendChild(element);
+
+        element.click();
+
+        document.body.removeChild(element);
+    }
 }
--- rules.php
+++ rules.php
@@ -5,27 +5,30 @@ require 'Includes/db_connect.php';
 <html>
     <head>
         <title>Rules Engine</title>
-		<link rel="stylesheet" type="text/css" href="CSS/layout_rules.css">
+        <link rel="stylesheet" type="text/css" href="CSS/layout_rules.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">
         </script>
     </head>
     <body>
         <script type="text/javascript" src="JS/rules.js"></script>
         <script type="text/javascript" src="JS/parser.js"></script>
-        <a href="index.php" id=home>DECENG Home</a><br/>
-        <form action="<?php $_PHP_SELF ?>" method="POST">
-            Enter your rules: <br/>
-            <input type="text" name="rule" required="required" id="rule_box"/> <br/> <br/>
-            <input type="text" name="test" required="required" id="test"/> <br/> <br/>
-            <p id="demo">
-            </p>
-            <input class=button type="submit" value="Process"/>
-        </form>
+        <input type="hidden" id="error_switch" value="Y">
+        <a href="index.php" id="home">DECENG Home</a><br/>
+        <input type="text" name="rule" required="required" id="rule_box"/> <br/><br/>
+        <div id="parse_json"> </div> <br/>
+        <div id="rule_out"
+            style="
+            color: green;
+            font-family: &quot;Comic Sans MS&quot;, cursive, sans-serif;">
+        </div> <br/>
+        <button type="button" id="parse_rule">Create Rule File</button>
         <script>
             $(document).ready(function(){
                 $("#rule_box").on('keyup paste', function(){
                     process_rule($("#rule_box").val());
                 });
+                $("#parse_rule").click(process_json);
+                $("#rule_out").click(download);
             });
         </script>
     </body>
